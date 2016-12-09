a43b3a4dc93b25d84cd79c953c0f5525150a81b1
Purva Dublay <purvadublay@gmail.com>
2016-12-08 22:20:29 -0500

    Add variable playgroung area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//   name: 'Purva',
+//   age: 21
+// }
+//
+// function updatePerson(obj) {
+//   // obj = {
+//   //       name: 'Purva',
+//   //       age: 24
+//   // };
+//   obj.age = 24;
+// }
+
+//updatePerson(person);
+//console.log(person);
+
+// array example
+
+var grades = [15, 17];
+
+function addGrades(grades){
+  grades.push(55);
+  debugger;
+}
+
+addGrades(grades);
+console.log(grades);
